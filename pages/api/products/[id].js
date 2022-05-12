3e165ff08930db6dd00a3991ba6640a1d2000078
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-05-12 15:24:57 -0400

    -

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -17,22 +17,5 @@ export default async function handler(req, res) {
             res.status(500).json(error)
         }
     }
-    // if (method === "PUT") {
-    //     try {
-    //         const product = await Product.create(req.body);
-    //         res.status(201).json(product);
-    //     } 
-    //     catch (error) {
-    //         res.status(500).json(error)
-    //     }
-    // }
-    // if (method === "DELETE") {
-    //     try {
-    //         const product = await Product.create(req.body);
-    //         res.status(201).json(product);
-    //     } 
-    //     catch (error) {
-    //         res.status(500).json(error)
-    //     }
-    // }
+    
 }
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,35 +4,59 @@ import Image from "next/image";
 import { useState } from "react";
 
 const Product = ({pizza}) => {
-    const [size, setSize] = useState(0);
+    const [price, setPrice] = useState(pizza.prices[0])
+    const [size, setSize] = useState(0)
+    const [quantity, setQuantity] = useState(1)
+    const [extras, setExtras] = useState([])
+    
+
+    const changePrice = (number) => {
+        setPrice(price + number)        
+    }    
+
+    const handleSize = (sizeIndex) => {
+        const difference = pizza.prices[sizeIndex] - pizza.prices[size]
+        setSize(sizeIndex)
+        changePrice(difference)
+    }
 
     const handleChange = (e, option) => {
-        const checked = e.target.checked;
+        const checked = e.target.checked
+        if (checked) {
+            changePrice(option.price)
+            setExtras((prev) => [...prev, option])
+        }
+        else {
+            changePrice(-option.price)
+            setExtras(extras.filter(extra => extra._id !== option._id))
+        }
     }
+
+    console.log(extras);
 
     return (
         <div className={styles.container}>
             <div className={styles.left}>
                 <div className={styles.imgContainer}>
-                    <Image src={pizza.img} alt={pizza.name} layout="fill" objectFit="contain" />   
+                    <Image src={pizza.img} alt={pizza.title} layout="fill" objectFit="contain" />   
                 </div>
             </div>
             <div className={styles.right}>
                 <h1 className={styles.title}>{pizza.title}</h1>
-                <span className={styles.price}>${pizza.prices[size]}</span>
+                <span className={styles.price}>${price}</span>
                 <p className={styles.desc}>{pizza.desc}</p>
 
                 <h3 className={styles.choose}>Choose the size</h3>
                 <div className={styles.sizes}>
-                    <div className={styles.size} onClick={()=>setSize(0)}>
+                    <div className={styles.size} onClick={()=>handleSize(0)}>
                         <Image src="/img/size.png" layout="fill" alt="size of pizza" />
                         <span className={styles.number}>Small</span>
                     </div>
-                    <div className={styles.size} onClick={()=>setSize(1)}>
+                    <div className={styles.size} onClick={()=>handleSize(1)}>
                         <Image src="/img/size.png" layout="fill" alt="size of pizza" />
                         <span className={styles.number}>Medium</span>
                     </div>
-                    <div className={styles.size} onClick={()=>setSize(2)}>
+                    <div className={styles.size} onClick={()=>handleSize(2)}>
                         <Image src="/img/size.png" layout="fill" alt="size of pizza" />
                         <span className={styles.number}>Large</span>
                     </div>
@@ -56,7 +80,12 @@ const Product = ({pizza}) => {
                 </div>
 
                 <div className={styles.add}>
-                    <input type="number" defaultValue={1} className={styles.quantity} />
+                    <input 
+                        type="number" 
+                        defaultValue={1} 
+                        className={styles.quantity} 
+                        onChange={(e)=>setQuantity(e.target.value)} 
+                    />
                     <button className={styles.button}>Add to Cart</button>
                 </div>
             </div>
