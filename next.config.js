d48296f173509e436760822af381691a0b3597dd
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-05-24 18:08:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
-  },
   images: {
-    domains: ["res.cloudinary.com", "cdn.shopify.com"]
+    domains: ["res.cloudinary.com"]
   }
 }
 
